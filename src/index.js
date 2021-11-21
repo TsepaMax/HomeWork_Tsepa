215e32c8d9d87758bd059a38d3e5457f950a2ae9
TsepaMax <tsepamax@yandex.ru>
2021-11-21 20:40:55 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,21 +1,68 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-const text = prompt("Введите Ваше сообщение:");
+const Input = () => {
+  const [messages, setMessages] = useState([]);
+  const [value, setValue] = useState("");
+  const [isFirstRender, setIsFirstRender] = useState(true);
 
-const Message = (props) => {
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  useEffect(() => {
+    // Просто проверка в консоли, создание записи происходит при нажатии кнопки Submit
+    console.log(value);
+  }, [value]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (value !== "") {
+      setMessages([...messages, { author: "Me", text: value }]);
+    }
+    setValue("");
+  };
+
+  useEffect(() => {
+    if (isFirstRender) {
+      setIsFirstRender(false);
+      return;
+    }
+    setTimeout(() => {
+      alert("Новое сообщение добавлено!");
+    }, 500);
+  }, [messages]);
+
   return (
-    <div className="message">
-      <h1>Добрый день!</h1>
-      <p>Вы ввели: {props.text}</p>
-    </div>
+    <React.Fragment>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Введите Ваше сообщение"
+          value={value}
+          onChange={handleChange}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <MessagesList messages={messages} />
+    </React.Fragment>
   );
+};
+
+const MessagesList = (props) => {
+  return props.messages.map((message) => (
+    <div>
+      <h2>Author: {message.author}</h2>
+      <p>Text: {message.text}</p>
+      <hr />
+    </div>
+  ));
 };
 
 ReactDOM.render(
   <React.StrictMode>
-    <Message text={text} />
+    <Input />
   </React.StrictMode>,
   document.getElementById("root")
 );
