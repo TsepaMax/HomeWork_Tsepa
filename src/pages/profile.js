a9242cfdcd4e6566ef9eda54b3ebee99013cc197
TsepaMax <tsepamax@yandex.ru>
2022-02-21 21:54:08 +0300

    commit

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,11 +1,34 @@
+import { useDispatch, useSelector } from "react-redux";
 import { useStyles } from "./use-styles";
+import { showNameToggle } from "../store/profile/actions";
+import { useCallback, useState } from "react";
 
 export const ProfilePage = () => {
   const styles = useStyles();
+  const [upd, setUpd] = useState(0);
+
+  const { showName, name } = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  const toShowName = useCallback(() => {
+    dispatch(showNameToggle);
+    setUpd((prevUpd) => prevUpd + 1);
+  }, [dispatch]);
 
   return (
     <div>
-      <p className={styles.homePageText}>ProfilePage</p>
+      <div>
+        <p className={styles.homePageText}>
+          My name is: {showName === true ? name : "******"}
+        </p>
+      </div>
+      <input
+        type="checkbox"
+        checked={showName}
+        value={showName}
+        onChange={toShowName}
+      />
+      <span className={styles.homePageText}>Show my name</span>
     </div>
   );
 };
